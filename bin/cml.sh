b0a1823364fbfbf7cb28a9f2cace897634edc5e2
SIO <sio.wtf@gmail.com>
2011-09-03 13:38:59 +0400

    Sat Sep  3 13:38:55 MSD 2011

--- bin/cml.sh
+++ bin/cml.sh
@@ -2,10 +2,49 @@
 # Блокировка экрана с показом заставки
 # Отключаются до ввода пароля: клавиатура, мышь, звук
 
-amixer sset Master mute
-(sleep 3; pgrep xlock && xset dpms force off)&
-xlock -mode atunnels -erasemode no_fade -timeelapsed -usefirst -lockdelay 3\
- -timeout 4 -font "-*-terminus-medium-r-normal-*-32-*-*-*-*-*-*-*"\
- -planfont "-*-terminus-medium-r-normal-*-32-*-*-*-*-*-*-*"\
- -background gray -icongeometry 256x256
-amixer sset Master unmute
+LOCKDELAY=3
+TIMESTART=`date +%s` #засекаем начало блокировки
+
+timecalc () {
+    #Принимает два значения в секундах, рассчитывает разницу между первым и вторым,
+    #выводит в человекочитаемом виде прошедшее время
+    
+    TIMESTART=$1
+    TIMESTOP=$2
+    
+    TIMEPASSED=$(( $2 - $1 ))
+    SECONDSPASSED=$(( $TIMEPASSED % 60 )) #символ % - целочисленное деление
+    MINUTESPASSED=$(( $TIMEPASSED % 3600 / 60 ))
+    HOURSPASSED=$(( $TIMEPASSED % 86400 / 3600 ))
+    DAYSPASSED=$(( $TIMEPASSED / 86400 ))
+    
+    [ 0 -eq $SECONDSPASSED ] || TIMESTRING="$SECONDSPASSED seconds"
+    [ 0 -eq $MINUTESPASSED ] || TIMESTRING="$MINUTESPASSED minutes $TIMESTRING"
+    [ 0 -eq $HOURSPASSED ] || TIMESTRING="$HOURSPASSED hours $TIMESTRING"
+    [ 0 -eq $DAYSPASSED ] || TIMESTRING="$DAYSPASSED days $TIMESTRING"
+    echo "$TIMESTRING"
+}
+
+# СОБСТВЕННО БЛОКИРОВКА
+
+    #отключение звука
+    amixer sset Master mute
+    
+    #отключение экрана через LOCKDELAY
+    (sleep $LOCKDELAY; pgrep xlock && xset dpms force off)&
+    
+    #заставка
+    xlock -mode atunnels -erasemode no_fade -timeelapsed -usefirst -lockdelay $LOCKDELAY\
+     -timeout 4 -font "-*-terminus-medium-r-normal-*-32-*-*-*-*-*-*-*"\
+     -planfont "-*-terminus-medium-r-normal-*-32-*-*-*-*-*-*-*"\
+     -background gray -icongeometry 256x256 
+    
+    #включение звука
+    amixer sset Master unmute 
+    
+TIMESTOP=`date +%s` #засекаем начало блокировки
+
+#уведомление о продолжительности блокировки
+notify-send -i info -u low "`hostname` unlocked" "`hostname` was locked for `timecalc $TIMESTART $TIMESTOP`"
+    
+    
