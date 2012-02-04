933c14977b335ce3d57f30fb010f03f9e3c412cf
SIO <sio.wtf@gmail.com>
2012-02-04 22:19:33 +0400

    Sat Feb  4 22:19:33 MSK 2012

--- bin/aptitude-updater.sh
+++ bin/aptitude-updater.sh
@@ -1,35 +1,47 @@
 #!/bin/bash
 
 # aptitude-updater.sh
+# A handy script to simplify dealing with multi-repo installation
 #
-#        Copyright 2012 SIO (sio.wtf@gmail.com)
-#        
-#        This program is free software; you can redistribute it and/or modify
-#        it under the terms of the GNU General Public License as published by
-#        the Free Software Foundation; either version 3 of the License, or
-#        (at your option) any later version.
-#        
-#        This program is distributed in the hope that it will be useful,
-#        but WITHOUT ANY WARRANTY; without even the implied warranty of
-#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#        GNU General Public License for more details.
-#        
-#        You should have received a copy of the GNU General Public License
-#        along with this program; if not, write to the Free Software
-#        Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#        MA 02110-1301, USA or visit <http://www.gnu.org/licenses/>.
+#    Copyright 2012 SIO (sio.wtf@gmail.com)
+#    
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 3 of the License, or
+#    (at your option) any later version.
+#    
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#    
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#    MA 02110-1301, USA or visit <http://www.gnu.org/licenses/>.
 #
 # Now playing: Destroy The Runner/Saints
 
 #set -e -u
 
+usage() {
+    echo -e "Usage: aptitude versions '\$pattern'|$0 {install|hold|remove|...}\nRefer to \`$0 --help' for more information"
+    exit
+}
+help_msg() {
+    echo -e "Usage: aptitude versions '\$pattern'|$0 {install|hold|remove|...}\n\nPossible parameters include: install, hold, remove, purge, markauto, reinstall, unhold, unmarkauto. Those parameters are passed to aptitude, so please refer to its documentation for further questions.\n\nExample: aptitude versions \"?upgradable\"|$0 install"
+    exit
+}
+
 # validate command-line parameters
 case "$*" in
     install|hold|remove|purge|markauto|reinstall|unhold|unmarkauto)
     ;;
+    -h|--help)
+        help_msg
+    ;;
     *)
-        echo "Usage: aptitude versions '\$pattern'|$0 {install|hold|remove|purge|markauto|reinstall|unhold|unmarkauto}"
-        exit
+        usage
     ;;
 esac
 
@@ -46,7 +58,7 @@ exec </dev/tty
 # parse input
 
 # separate input into CHUNKs with predefined SEPARATOR line
-# ugly code
+# ugly code below
 SEPARATOR="^Package" #param
 
 LINE_NUMBERS=`echo "$PIPE_INPUT"|grep -En $SEPARATOR|cut -d: -f1`
@@ -60,10 +72,11 @@ do
     CHUNK[$i]=`echo "$PIPE_INPUT"|head -n $LAST_LINE|tail -n $(($LAST_LINE-$FIRST_LINE))`
     PACKAGE[$i]=`echo "${CHUNK[$i]}"|head -n 1|cut -d\  -f2|cut -d: -f1`
 done
+[[ -z $PIPE_INPUT || -z ${PACKAGE[1]} ]] && echo "Bad input was given. Exiting..." && usage
 
-#show user chunks one by one and ask for his decision
-ACTION="$1" #todo: exceptions
-DEFAULT_ANSWER=n #param
+#show user chunks one by one and ask for his decisions
+ACTION="$1" 
+DEFAULT_ANSWER=y #param
 
 for i in `seq 1 $MAX_ITERATOR`
 do
@@ -85,8 +98,18 @@ do
     done    
 done
 
-# show positive answers
+# show positive answers and launch aptitude
+PACKAGES_TOGO=""
 for i in `seq 1 $MAX_ITERATOR`
 do
-    [ "${ANSWER[$i]}" = "y" ] && echo ${PACKAGE[$i]}
+    [ "${ANSWER[$i]}" = "y" ] && PACKAGES_TOGO="$PACKAGES_TOGO ${PACKAGE[$i]}"
 done
+if [ -z $PACKAGES_TOGO ]
+then
+    echo -e "\nNo action will be performed"
+else
+    echo -e "\nWill now perform \"aptitude $ACTION$PACKAGES_TOGO\""
+    echo -n "Press Enter to proceed or ^C to abort..."
+    read || exit
+    sudo aptitude $ACTION $PACKAGES_TOGO
+fi
